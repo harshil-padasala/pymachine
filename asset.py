4c200e5f9a23842a9f87e996e8bd60bb998e1473
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-09-13 18:30:05 +0530

    added information about project in read me file and added some images and with that added a litte script that can be used

--- asset.py
+++ asset.py
@@ -1,17 +1,21 @@
 import mysql.connector
 from mysql.connector import Error
+from datetime import datetime
 
 
 def connect():
     """ Connect to MySQL database """
     conn = None
     try:
-        conn = mysql.connector.connect(host='localhost',
-                                       database='python_mysql',
-                                       user='root',
-                                       password='SecurePass1!')
+        conn = mysql.connector.connect(host='localhost', database="asset_manager", user='root', password='')
         if conn.is_connected():
+            dt = datetime.now()
             print('Connected to MySQL database')
+            my_cursor = conn.cursor()
+            my_cursor.execute(f"INSERT INTO `tbl_active_asset`(asset_id,date_time) VALUES ('3','{dt}')")
+            conn.commit()
+            for i in my_cursor:
+                print(i)
 
     except Error as e:
         print(e)
--- pymachine/README.md
+++ pymachine/README.md
@@ -1,2 +1,27 @@
 # pymachine
-gives the information about user sytem 
+
+Pymachine is a python library that gives the information about user sytem and other information about system such as
+- ###Anti viruses information
+  - how many are installed?
+  - which are installed?
+  - Helpful website to convert and efficiently see Text to JSON fomate
+    - `https://jsonformatter.curiousconcept.com/`
+  ![img.png](Images/img.png)
+- ###Full system information
+  - os name
+  - os version
+  - os build type
+  - system model
+  - etc.
+  ![img_1.png](Images/img_1.png)
+- ###Network information
+  - ip configuration
+  - NIC information and configuration
+  - etc.
+  ![img_2.png](Images/img_2.png)
+- ###System Information
+  - model
+  - version
+  - processor
+  - system type (x64,x32)
+  ![img_3.png](Images/img_3.png)
