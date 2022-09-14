16455d182e760a29211dbfa180c089d2219be9e1
harshil <harshilpadsala999@gmail.com>
2022-09-14 22:40:54 +0530

    added .gitignore and modified system_information.py

--- pymachine/pymachine/system_information.py
+++ pymachine/pymachine/system_information.py
@@ -8,19 +8,13 @@ def get_system_info_dict():
     my_system = c.Win32_ComputerSystem()[0]
     """Return the system information."""
     return {
-        'platform': platform.platform(),
-        'system': platform.system(),
-        'node': platform.node(),
-        'release': platform.release(),
-        'version': platform.version(),
-        'machine': platform.machine(),
-        'processor': platform.processor(),
-        'manufacturer': my_system.Manufacturer,
-        'modal': my_system.Model,
-        'name': my_system.Name,
-        'NumberOfProcessors': my_system.NumberOfProcessors,
-        'systemType': my_system.SystemType,
-        'systemFamily': my_system.SystemFamily
+        'system': {
+            'system_name': platform.system(),
+            'node': platform.node(),
+            'system_version': platform.version(),
+            'processor': platform.processor(),
+            'system_type': my_system.SystemType,
+        }
     }
 
 
